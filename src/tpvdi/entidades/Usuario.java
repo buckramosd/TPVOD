f95af9cf43b4809197389336afac5fbdd3bf681a
buckramosd <buckramosd@gmail.com>
2023-02-22 14:26:33 +0100

    POJO Usuario

--- src/tpvdi/entidades/Usuario.java
+++ src/tpvdi/entidades/Usuario.java
@@ -9,11 +9,72 @@ package tpvdi.entidades;
  * @author damm
  */
 public class Usuario {
-    String nombreUsuario;
     
-    String contraseña;
+    private String nombreUsuario;
     
-    String rol;
+    private String contraseña;
+    
+    private String rol;
+    
+    private String nombre;
+    
+    private String apellidos;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombreUsuario, String contraseña, String rol, String nombre, String apellidos) {
+        this.nombreUsuario = nombreUsuario;
+        this.contraseña = contraseña;
+        this.rol = rol;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombreUsuario=" + nombreUsuario + ", contrase\u00f1a=" + contraseña + ", rol=" + rol + ", nombre=" + nombre + ", apellidos=" + apellidos + '}';
+    }
     
     
     
