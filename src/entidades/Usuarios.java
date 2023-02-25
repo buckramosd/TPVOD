c9b298cf9aedf0324e86aa60b9213399273607dc
Óscar Martín Sevilla <oscarmartinsev@gmail.com>
2023-02-25 12:44:09 +0100

    Usuarios donete
    getUsuario por nombreUsuario que es único

--- src/entidades/Usuarios.java
+++ src/entidades/Usuarios.java
@@ -1,7 +1,38 @@
 
 package entidades;
 
+import java.util.ArrayList;
+
 
 public class Usuarios {
+    private ArrayList<Usuario> listaUsuarios;
+
+    public Usuarios() {
+        listaUsuarios = new ArrayList();
+    }
     
+    public void addUsuario(Usuario user){
+        listaUsuarios.add(user);
+    }
+    
+    public Usuario getUsuario(String nombreUsuario) {
+        Usuario encontrado = null;
+        
+        for(int i = 0; i < listaUsuarios.size(); i++) {
+            // Guardo en un string el nombre de los usuarios de la lista
+            String compara = listaUsuarios.get(i).getNombreUsuario();
+            if(compara.equals(nombreUsuario)) {
+                encontrado = listaUsuarios.get(i);
+            }
+        }
+        return encontrado;
+    }
+    
+    public boolean removeUsuario(String nombreUsuario) {
+        return listaUsuarios.remove(this.getUsuario(nombreUsuario));
+    }
+    
+    public int size() {
+        return listaUsuarios.size();
+    }
 }
