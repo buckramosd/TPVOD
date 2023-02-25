d659cd11b57663014a0770be955cbf3eb22af5fa
Óscar Martín Sevilla <oscarmartinsev@gmail.com>
2023-02-25 12:53:34 +0100

    Productos creado y terminado
    Busqueda por int idProducto, la primaria de la entidad

---
+++ src/entidades/Productos.java
@@ -0,0 +1,29 @@
+
+package entidades;
+
+import java.util.ArrayList;
+
+
+public class Productos {
+    private ArrayList<Producto> listaProductos;
+
+    public Productos() {
+        listaProductos = new ArrayList();
+    }
+    
+    public void addProducto(Producto producto){
+        listaProductos.add(producto);
+    }
+    
+    public Producto getProducto(int idProducto) {
+        return listaProductos.get(idProducto);
+    }
+    
+    public boolean removeEmpleado(int idProducto) {
+        return listaProductos.remove(this.getProducto(idProducto));
+    }
+    
+    public int size() {
+        return listaProductos.size();
+    }
+}
