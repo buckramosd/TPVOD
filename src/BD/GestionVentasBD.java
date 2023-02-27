4f0a2d4b66e48b829ddcd3cff648ddfbe01e3798
Óscar Martín Sevilla <oscarmartinsev@gmail.com>
2023-02-27 12:10:11 +0100

    Gventas terminado?

--- src/BD/GestionVentasBD.java
+++ src/BD/GestionVentasBD.java
@@ -1,4 +1,3 @@
-
 package BD;
 
 import entidades.Producto;
@@ -7,13 +6,14 @@ import entidades.Venta;
 import entidades.Ventas;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
 public class GestionVentasBD {
-   private final String HOST;
+
+    private final String HOST;
     private final String USUARIO;
     private final String PASSWORD;
     private final String BD;
@@ -27,19 +27,19 @@ public class GestionVentasBD {
         this.BD = BD;
         this.PUERTO = PUERTO;
     }
-    
+
     /**
      * Método para insertar una nueva venta en la base de datos.
      * @param venta
-     * @return 
+     * @return
      */
-    public boolean insertarVenta(Venta venta){
-        boolean resultado = false;       
+    public boolean insertarVenta(Venta venta) {
+        boolean resultado = false;
         try {
             conectar();
             Statement sentencia = conexion.createStatement();
             String sql = String.format("INSERT INTO venta(codVenta, cantidad, idProducto, username) VALUES ('%s', '%s','%s', '%s')",
-                   venta.getCodVenta(),venta.getCantidad(), venta.getProducto().getIdProducto(), venta.getUsuario().getNombreUsuario());
+                    venta.getCodVenta(), venta.getCantidad(), venta.getProducto().getIdProducto(), venta.getUsuario().getNombreUsuario());
             System.out.println("Consulta SQL: " + sql);
             resultado = sentencia.execute(sql);
             sentencia.close();
@@ -49,7 +49,50 @@ public class GestionVentasBD {
         }
         return resultado;
     }
-    
+
+    /**
+     * Método para buscar una venta por su id
+     * @param venta
+     * @return 
+     */
+    public Venta buscarVenta(Venta venta) {
+        Venta ventaBuscada = null;
+        ResultSet rs;
+        try {
+            conectar();
+            Statement sentencia = conexion.createStatement();
+            String sql = String.format("SELECT ventas.* FROM ((ventas "
+                    + "INNER JOIN productos ON ventas.idProducto = productos.idProducto)"
+                    + "INNER JOIN usuarios ON ventas.username = usuarios.username)"
+                    + "WHERE idVenta='%s'",
+                    venta.getIdVenta());
+            System.out.println("Consulta SQL: " + sql);
+            sentencia.execute(sql);
+            rs = sentencia.getResultSet();
+            while (rs.next()) {
+                ventaBuscada = new Venta(rs.getInt(2),
+                        rs.getInt(3),
+                        rs.getInt(4),
+                        new Producto(rs.getInt(5), rs.getString(6), rs.getDouble(7), rs.getInt(8)),
+                        new Usuario(rs.getString(9), rs.getString(10), rs.getString(11), rs.getString(12), rs.getString(13)
+                        ));
+                return ventaBuscada;
+            }
+            sentencia.close();
+            desconectar();
+        } catch (SQLException ex) {
+            System.out.println("Error en conexión(Buscar producto)" + ex.getMessage());
+        }
+
+        return ventaBuscada;
+    }
+
+    /**
+     * 
+     * @param venta
+     * @param venta_new
+     * @return 
+     */
     public boolean actualizarVenta(Venta venta, Venta venta_new) {
         boolean resultado = false;
         try {
@@ -62,18 +105,48 @@ public class GestionVentasBD {
             resultado = sentencia.execute(sql);
             sentencia.close();
             desconectar();
-        } catch (SQLException ex){
+        } catch (SQLException ex) {
             System.out.println("Error en conexión (Actualizar venta)" + ex.getMessage());
         }
         return resultado;
     }
-    /*
+
     /**
-     * Método que devuelve una lista de tipo venta con todas las ventas de la base de datos.
+     * Método para modificar cantidades a un pedido/venta.
+     * @param venta
+     * @param cantidad
      * @return 
      */
-    public Ventas listarTodasVentas(){
-        Ventas listado = new Ventas();
+    public boolean modificarCantidadVenta(Venta venta, int cantidad) {
+        boolean resultado = false;
+        try {
+            conectar();
+            String sql = String.format("UPDATE ventas SET cantidad=? WHERE idVenta=? AND codVenta=?");
+            PreparedStatement pstmt = conexion.prepareStatement(sql);
+            
+            Venta buscada = buscarVenta(venta);
+            int cantidadInicial = buscada.getCantidad();
+            int cantidadTotal = cantidadInicial + cantidad;
+            
+            System.out.println("Consulta SQL: " + sql);
+            pstmt.setInt(1,cantidadTotal);
+            pstmt.setInt(2,venta.getIdVenta());
+            pstmt.setInt(3,venta.getCodVenta());
+            pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("Error en conexión (Actualizar venta)" + ex.getMessage());
+        }
+        return resultado;
+    }
+
+    /**
+     * Método que devuelve una lista de tipo venta con todas las ventas de la
+     * base de datos.
+     *
+     * @return
+     */
+    public Ventas listarTodasVentas() {
+        Ventas listadoCompleto = new Ventas();
         ResultSet rs;
         try {
             conectar();
@@ -83,25 +156,32 @@ public class GestionVentasBD {
                     + "INNER JOIN usuarios ON ventas.username = usuarios.username)");
             sentencia.execute(sql);
             rs = sentencia.getResultSet();
-            while(rs.next()){
-                listado.addVenta(new Venta());
+            while (rs.next()) {
+                listadoCompleto.addVenta(new Venta(rs.getInt(1),
+                        rs.getInt(2),
+                        rs.getInt(3),
+                        new Producto(rs.getInt(4), rs.getString(5), rs.getDouble(6), rs.getInt(7)),
+                        new Usuario(rs.getString(8), rs.getString(9), rs.getString(10), rs.getString(11), rs.getString(12)
+                        )));
             }
             rs.close();
             sentencia.close();
             desconectar();
-        } catch(SQLException ex){
+        } catch (SQLException ex) {
             System.out.println("Error conexión" + ex.getMessage());
         }
-        return listado;
+        return listadoCompleto;
     }
-    
+
     /**
-     * Método que devuelve una lista de tipo venta con todas las ventas con cierto codVenta de la base de datos.
+     * Método que devuelve una lista de tipo venta con todas las ventas con
+     * cierto codVenta de la base de datos.
+     *
      * @param codVenta
      * @return lista
      */
-    public Ventas listarCiertasVentas(int codVenta){
-        Ventas listado = new Ventas();
+    public Ventas listarCiertasVentas(int codVenta) {
+        Ventas listadoCod = new Ventas();
         ResultSet rs;
         conectar();
         try {
@@ -112,24 +192,24 @@ public class GestionVentasBD {
                     + "WHERE codVenta='%s'", codVenta);
             sentencia.execute(sql);
             rs = sentencia.getResultSet();
-            while(rs.next()){
-                listado.addVenta(new Venta(rs.getInt(2), 
-                                           rs.getInt(3), 
-                                           rs.getInt(4),
-                                           new Producto(rs.getInt(5),rs.getString(6), rs.getDouble(7),rs.getInt(8)),
-                                           new Usuario(rs.getString(9),rs.getString(10),rs.getString(11),rs.getString(12),rs.getString(13)
-                                           )));
+            while (rs.next()) {
+                listadoCod.addVenta(new Venta(rs.getInt(1),
+                        rs.getInt(2),
+                        rs.getInt(3),
+                        new Producto(rs.getInt(4), rs.getString(5), rs.getDouble(6), rs.getInt(7)),
+                        new Usuario(rs.getString(8), rs.getString(9), rs.getString(10), rs.getString(11), rs.getString(12)
+                        )));
             }
             rs.close();
             sentencia.close();
             desconectar();
-        } catch(SQLException ex){
+        } catch (SQLException ex) {
             System.out.println("Error conexión" + ex.getMessage());
         }
-        
-        return listado;
+
+        return listadoCod;
     }
-     
+
     private boolean conectar() {
         boolean resultado = true;
         try {
